1587020b2a224659975c8fd0a6a7cec06d67325a
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-17 00:28:49 +0100

    Create a signIn action

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,3 +22,17 @@ export const signout = () => {
 		payload: ''
 	};
 };
+
+export const signIn = (formProps, callback) => async dispatch => {
+	try {
+		const response = await axios.post('http://localhost:3090/signin', formProps);
+		dispatch({ type: AUTH_USER, payload: response.data.token });
+		localStorage.setItem('token', response.data.payload);
+		callback();
+	} catch (e) {
+		dispatch({
+			type: AUTH_ERROR,
+			payload: 'Invalid login credentials'
+		});
+	}
+};
