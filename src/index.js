ec1039affb323437a38f9fbf8826fdc17d77b7dc
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-16 14:53:30 +0100

    Add BrowserRouter, Route, Provider createStore and other components in index.js filrfor creating routes that houses the welcome page and sign up page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from 'components/App';
+import Welcome from 'components/Welcome';
+import SignUp from 'components/auth/SignUp';
+import reducers from 'components/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={createStore(reducers, {})}>
+		<BrowserRouter>
+			<App>
+				<Route path="/" exact component={Welcome} />
+				<Route path="/signup" exact component={SignUp} />
+			</App>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
