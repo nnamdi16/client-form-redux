20499d191e613431c71bc60744bd1a7e71dcf536
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-16 22:07:48 +0100

    Move file from component folder

--- src/components/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import auth from 'components/reducers/auth';
-
-export default combineReducers({
-	auth,
-	form: formReducer
-});
